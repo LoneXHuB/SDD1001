5ded20c073c89048617d178e75e6ff73b5af95d6
LoneX <elouanes.khelifi@uqtr.ca>
2023-11-17 21:46:34 -0500

    first commit

---
+++ client.py
@@ -0,0 +1,35 @@
+import requests
+import json 
+api_url = 'http://localhost:5000/get_clients'
+add_url = 'http://localhost:5000/add_client'
+
+def request_clients():    
+    response = requests.post(api_url, json={'table_name' : 'clients'})
+
+    if response.status_code == 200:
+        data = response.json()
+        table_data = json.loads(data['table_data'])
+
+        print("Liste Clients : \n")
+
+        for row in table_data:
+            print(f"ID: {row['id']} ,  Name : {row['name']} , Age: {row['age']}")
+
+    else:
+        print(f"Error: {response.status_code} = {response.json()['error']}")
+
+def request_add_client():
+    client_row = {'id':5 , 'name': 'Doe' , 'age' : 33} 
+
+    response = requests.post(add_url, json={"new_row": client_row})
+
+    if response.status_code == 200:
+        print(response.json()['message'])
+    else:
+        print(f"Error! : {response.status_code} = {response.json()['error']}")
+
+if __name__ == "__main__":
+    request_clients()
+    request_add_client()
+    request_clients()
+    
---
+++ server.py
@@ -0,0 +1,53 @@
+from flask import Flask, request, jsonify
+import pandas as pd
+import os
+
+file_directory = os.path.dirname(os.path.abspath(__file__))
+
+os.chdir(file_directory)
+
+print("directory : ", os.getcwd())
+
+app = Flask(__name__)
+
+csv_data = {
+    "clients" : pd.read_csv("clients.csv"),
+    "prices": pd.read_csv("prices.csv")
+}
+
+@app.route('/get_clients', methods= ['POST'])
+def get_clients():
+    data = request.get_json()
+
+    if 'table_name' not in data:
+        return jsonify({'error' : 'Table name not provided!'}) , 400
+
+    table_name = data['table_name']
+
+    if table_name not in csv_data:
+        return jsonify({'error':'table not found!'}), 404
+    
+    table_df = csv_data[table_name]
+    table_json = table_df.to_json(orient='records') 
+
+    return jsonify({'table_data' : table_json})
+
+
+@app.route('/add_client', methods= ['POST'])
+def add_client():
+    #get client info 
+    data = request.get_json()
+
+    if 'new_row' not in data:
+        return jsonify({'error' : 'No row provided'})
+    
+    new_client = data['new_row']
+    
+    #lire le fichier csv et y ajouter le nouveau client
+    csv_data['clients'] = csv_data['clients'].append(new_client, ignore_index=True)
+
+    csv_data['clients'].to_csv('clients.csv')
+
+    return jsonify({'message': "Row added successfully!"})
+
+app.run(port=5000)
